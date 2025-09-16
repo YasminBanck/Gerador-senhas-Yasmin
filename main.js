fa7d4c250dbcdd4d79108ca582f7588b7cdcd177
yasmin Banck <145784815+YasminBanck@users.noreply.github.com>
2025-09-16 11:41:49 -0300

    16/09

--- main.js
+++ main.js
@@ -65,20 +65,21 @@ function geraSenha() {
 
 function classificaSenha() {
 
-let entropia = tamanhoSenha *Math.log2(tamanhoSenha);
+    let entropia = tamanhoSenha*Math.log2(tamanhoSenha);
 
     forcaSenha.classList.remove('fraca', 'media', 'forte');
     if (entropia > 57) {
         forcaSenha.classList.add('forte');
     } else if (entropia > 35 && entropia < 57) {
         forcaSenha.classList.add('media');
-    } else if (tamanhoSenha <= 5) {
+    } else if (entropia <= 35) {
         forcaSenha.classList.add('fraca');
 
     }
 
-const valorEntropia = document.querSelector ('.entropia');
-valorEntropia.textContent = "Um computador pode levar até " +
-Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
+    const valorEntropia = document.querySelector('.entropia');
+    valorEntropia.textContent = "Um computador pode levar até " +
+    Math.floor(2**entropia/(100e6*60*24)) + " dias para descobrir essa senha.";
+
 
 }
